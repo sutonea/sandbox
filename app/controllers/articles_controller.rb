0a582bd8514c166f8cfb08948802a3e8038f98e1
sutonea <stonhair@gmail.com>
2022-11-12 22:02:18 +0900

    Destroy a record with Turbo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,8 +36,7 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to articles_url }
     end
   end
 
